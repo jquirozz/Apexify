ca309dcc88fa034ee70a582402ee2510d97e2a66
Joseph <jq84144@gmail.com>
2024-01-19 03:11:33 -0500

    Navigation system for calendar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,60 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
-import NotFound from './pages/NotFound'
-import Home from './pages/Home'
-
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Outlet,
+  useParams,
+  NavLink
+} from 'react-router-dom'
 import NavBar from './components/NavBar'
+import { useEffect, useState } from 'react'
 
-import './App.scss'
+function Calendar () {
+  return (
+    <div>
+      <h1>Calendar</h1>
+      <h2>Years</h2>
+      <NavLink to='/calendar/2023'>2023</NavLink>
+      <NavLink to='/calendar/2024'>2024</NavLink>
+      <Outlet />
+    </div>
+  )
+}
+
+function Rounds () {
+  const { yearId } = useParams()
+
+  useEffect(() => {
+    console.log('YEAR', yearId)
+  }, [yearId])
+
+  return (
+    <div>
+      <h2>Rounds</h2>
+      <NavLink to={`/calendar/${yearId}/1`}>Round 1</NavLink>
+      <NavLink to={`/calendar/${yearId}/2`}>Round 2</NavLink>
+      <Outlet />
+    </div>
+  )
+}
+
+function Info () {
+  const { yearId, roundId } = useParams()
+  const [param, setParam] = useState({ year: yearId, round: roundId })
+  const { year, round } = param
+
+  useEffect(() => {
+    setParam({ year: yearId, round: roundId })
+  }, [yearId, roundId, setParam])
+
+  return (
+    <div>
+      <h2>
+        Info: {year} / Round {round}
+      </h2>
+    </div>
+  )
+}
 
 function App () {
   return (
@@ -13,8 +62,11 @@ function App () {
       <BrowserRouter>
         <NavBar />
         <Routes>
-          <Route path='*' element={<NotFound />} />
-          <Route path='/' element={<Home />} />
+          <Route path='/calendar' element={<Calendar />}>
+            <Route path=':yearId' element={<Rounds />}>
+              <Route path=':roundId' element={<Info />} />
+            </Route>
+          </Route>
         </Routes>
       </BrowserRouter>
     </div>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,13 @@
+import { NavLink } from 'react-router-dom'
 import './style/NavBar.scss'
 
 function NavBar () {
-  return <div className='NavBar'>NavBar</div>
+  return (
+    <div className='NavBar'>
+      <NavLink to='/'>Home</NavLink>
+      <NavLink to='/calendar'>Calendar</NavLink>
+    </div>
+  )
 }
 
 export default NavBar
